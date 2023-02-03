0791caf8283af18a2ea2ee0f7afe02c4c055bd71
Damen Birtola <damen.birtola@disruptivegames.com>
2023-02-02 19:27:59 -0800

    Initial unreal commit

---
+++ Source/Mary/MaryGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaryGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMaryGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMaryGameMode();
+};
+
+
+
