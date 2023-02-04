90567875af64561006edee284f4d84244e7b8bd2
Damen Birtola <damen.birtola@disruptivegames.com>
2023-02-03 23:07:48 -0800

    ASC interface on character

--- Source/Mary/MaryCharacter.cpp
+++ Source/Mary/MaryCharacter.cpp
@@ -11,6 +11,7 @@
 #include "EnhancedInputSubsystems.h"
 #include "Engine/LocalPlayer.h"
 #include "GameFramework/PlayerController.h"
+#include "MaryPlayerState.h"
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -40,6 +41,15 @@ AMaryCharacter::AMaryCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+}
+
+UAbilitySystemComponent* AMaryCharacter::GetAbilitySystemComponent() const
+{
+	if (AMaryPlayerState* MaryPS = GetPlayerState<AMaryPlayerState>())
+	{
+		return MaryPS->GetAbilitySystemComponent();
+	}
+	return nullptr;
 }
 
 void AMaryCharacter::BeginPlay()
